dc7e49adeec8567aecf7052a9768ac724bb0f038
Abdi Hassan <abdihassan@Abdis-MacBook-Air.local>
2025-01-21 11:09:29 +0100

    quiz

--- quiz.py
+++ quiz.py
@@ -1,13 +1,3 @@
-#!/usr/bin/env python
-from distutils.core import setup
-setup(
- name='quiz-abdirahman-1',
- version='1.0',
- author='Abdirahman-Hassan-Mahammed',
- license='MIT',
- long_description=open('README.md').read(),
-)
-
 import random
 
 def quiz_game():
